21c57bed115dd4724b95e4710655faaed7315ea1
BJH9 <tfzangin9@gmail.com>
2021-10-13 20:54:06 +0900

    add main lines for clami

--- app/src/main/java/edu/handong/csee/isel/cla/Main.java
+++ app/src/main/java/edu/handong/csee/isel/cla/Main.java
@@ -4,27 +4,38 @@ import weka.core.Instances;
 
 public class Main {
 	private Instances instancesCLA;
+	private Instances instancesCLAMI;
+
 	public static void main(String[] args) {
 		new Main().runner(args);
 	}
-	
+
 	void runner(String[] args) {
 		CLA cla = new CLA();
-		
+		CLAMI clami = new CLAMI();
+
+		System.out.println("--------unlabeled dataset----------");
+		System.out.println("");
 		System.out.println(Util.loadArff(args[0]));
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
+		for (int i = 0; i < 5; i++)
+			System.out.println("");
+
 		instancesCLA = Util.loadArff(args[0]);
 		cla.doClustering(instancesCLA, 50.0);
+		System.out.println("-----------cla result-----------");
+		System.out.println("");
 		System.out.println(cla.labelOnInstances(instancesCLA, 50.0, "FALSE"));
+		for (int i = 0; i < 5; i++)
+			System.out.println("");
 		
+		instancesCLAMI = Util.loadArff(args[0]);
+		System.out.println("---------clami result----------");
+		System.out.println("");
+		clami.doClustering(instancesCLAMI, 50.0);
+		clami.labelOnInstances(instancesCLAMI, 50.0, "FALSE");
+		clami.selectMetrices(instancesCLAMI);
+		System.out.println(clami.selectInstances(instancesCLAMI));
+
 	}
-	
-	
 
 }
